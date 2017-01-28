5a9713c21e239fb5f9e4da48e5e408a47f395e8c
LFonz <cllin7787@gmail.com>
2017-01-29 00:40:51 +1100

    Finished game logic, reset board, and reset score

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,24 +1,58 @@
 console.log("JS file is connected to HTML! Woo!")
 
-		var cardOne = 'queen';
-		var cardTwo = 'queen';
-		var cardThree = 'king';
-		var cardFour = 'king';
+		var cards = ['queen', 'queen', 'king', 'king'];
+		var cardsInPlay = [];
+		var gameBoard=document.getElementById('game-board');
+		var score = 0;
 
-		if (cardThree === cardFour) {
-			alert('You found a match!');
-		} else {
-			alert('Sorry, try again.');
-		}
-
-		var createCards = function() {
-			var gameBoard=document.getElementById('game-board');
-
-			for (i = 0; i < 4; i++) {
+		var createBoard = function() {
+			for (i = 0; i < cards.length; i++){
 				var newCard=document.createElement('div');
 				newCard.className="card";
+				newCard.setAttribute('data-card', cards[i]);
+				newCard.addEventListener('click', isTwoCards);
+				
 				gameBoard.appendChild(newCard);
+				gameBoard.appendChild(newCard);
+				}
 			}
-		}
-		createCards();
 
+
+		var isTwoCards = function() {
+			cardsInPlay.push(this.getAttribute('data-card'));
+				console.log(this.getAttribute('data-card'));
+				if (this.getAttribute('data-card') === 'queen') {
+					this.innerHTML = '<img src="queen.png" alt="Queen of Hearts" />';	
+					} else {
+					this.innerHTML = '<img src="king.png" alt="King of Spades" />';
+					}
+			if (cardsInPlay.length === 2) {
+				isMatch(cardsInPlay);
+				cardsInPlay = [];
+				}
+		}	
+
+		var isMatch = function(cards) {
+			if (cardsInPlay[0] === cardsInPlay[1]) {
+				alert('You found a match!');
+				score += 1;
+			} else {
+				alert('Sorry, try again.');
+			}
+			alert('score:' + score)
+		}
+
+		var resetBoard = function() {
+			var resetCard=document.getElementsByTagName('img');
+			while (resetCard.length > 0) {
+					resetCard[0].parentNode.removeChild(resetCard[0]);
+			}
+
+		}
+		createBoard();
+
+
+
+
+
+		
